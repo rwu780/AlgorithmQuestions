e7c511fe6f011f1ebd9bfd98df910e62a0a9441f
James <rwu780@gmail.com>
2018-08-24 15:18:40 -0400

    Add files via upload

--- Q16.java
+++ Q16.java
@@ -27,13 +27,23 @@ public class Q16 {
 		topRoot = new Node(0);
 		topRoot.left = new Node(1);
 		topRoot.right = new Node(1);
-//		topRoot.left.left = new Node(2);
-//		topRoot.right.right = new Node(2);
+		topRoot.left.left = new Node(2);
+//		topRoot.left.left.left = new Node(5);
+		topRoot.right.right = new Node(2);
 		topRoot.left.right = new Node(3);
 		topRoot.right.left = new Node(3);
+//		topRoot.right.right.right = new Node(5);
+
 	}
 	
 	public boolean isMirror(Node root1, Node root2){
+		
+		if(root1 == null && root2 == null){
+			return true;
+		}
+		else if(root1 == null || root2 == null){
+			return false;
+		}
 		
 		if(root1.value != root2.value){
 			return false;
@@ -44,12 +54,9 @@ public class Q16 {
 				root2.left == null &&
 				root2.right == null)
 			return true;
-		if(root1.left.value == root2.right.value && root2.left.value == root1.right.value){
-			return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
-		}
-		else{
-			return false;
-		}
+		
+		return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+
 	}
 	
 	public static void main(String[] args) {
